dc3c203dbcb19fe591caa1fdeb3191faac186eab
Arthur Cloche <arthurcloche@gmail.com>
2023-08-26 14:55:09 -0400

    feat:add readme

--- src/p5.letterSpacing.js
+++ src/p5.letterSpacing.js
@@ -1,11 +1,9 @@
 p5.prototype.letterSpacing = function (spacing = 0) {
     const ctx = drawingContext;
-    // console.log(ctx)
     if (drawingContext.constructor.name !== "CanvasRenderingContext2D") {
       console.log("Unfortunately, this feature is only available in 2D");
       return;
     }
-    //just making sure we are passing full pixels
     ctx.letterSpacing = `${spacing}px`;
     return ctx.letterSpacing;
   };
