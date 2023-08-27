61a2b6f3091f3052afd6b14fd89676f7b20a5e6c
Arthur Cloche <arthurcloche@gmail.com>
2023-08-27 13:24:23 -0400

    fix: fix readme typos and add example image

--- readme.md
+++ readme.md
@@ -1,12 +1,17 @@
 # p5.TextSpacing
-Taking advantage of the built-it functions in the webCanvas 2D drawing context that let us interact with the spacing of words and letter within a string. The direction is set by the text alignement. Works with imported font and CSS fonts aswell.
 
-Either clone this repository or just grab the function in the `src` folder.
+![](example.png)
+
+Taking advantage of the built-it functions in the webCanvas 2D drawing context that let us interact with the spacing of words and letter within a string in p5.js. The direction is set by the text alignement. Works with imported font and CSS fonts aswell.
+
+Either clone this repository or just grab the functions in the `src` folder.
 
 ## Important
+
 This works only in 2D because the function are native to the 2D drawing context API. It can be done in 3D but it would require a totally different setup.
 
 ## Methods
+
 ``` js
  function wordSpacing(space:number)
 ```
--- sketch.js
+++ sketch.js
@@ -1,75 +1,112 @@
-function setup() {
+    function setup() {
     createCanvas(800, 800);
     textAlign(CENTER, CENTER);
+    rectMode(CENTER)
     textSize(60);
-}
-
-function draw() {
+  }
+  
+  function draw() {
     clear();
     background(220);
-
+  
     const numframes = 2 * 60;
     const progress = (frameCount % numframes) / numframes;
-    const movement = sin(progress * TWO_PI) * 0.5 + 0.5;
+    const movement = ease((sin(progress * TWO_PI) * 0.5 + 0.5),8);
     const txt = "the quick something\njumped over what ?";
-    const linesOfWords = txt.split("\n");
+    
     const margins = 10 * 2;
-
-    linesOfWords.forEach((lineOfWords, index) => {
-        push();
-        const words = Math.max(0, lineOfWords.split(" ").length - 1);
-        const spacer = floor(
-            (width - margins - textWidth(lineOfWords)) / words
-        );
-        const ctx = drawingContext;
-        wordSpacing(spacer * movement);
-        text(lineOfWords, width / 2, textLeading() / 2 + textLeading() * index);
-        pop();
+    const justifiedMovement = true
+    
+    if(!justifiedMovement){
+    push()
+    translate(width/2,textLeading())
+    wordSpacing(60 * movement);
+    text(txt,0,0)
+    pop()
+    
+    push()
+    translate(width/2,height/2)
+    wordSpacing(10 * movement)*.25;
+    letterSpacing(10 * movement)*.25;
+    text(txt,0,0)
+    pop()
+    
+    push()
+    translate(width/2,height - textLeading())
+    letterSpacing(10 * movement);
+    text(txt,0,0)
+    pop()
+    } else {
+      const linesOfWords = txt.split("\n");
+      linesOfWords.forEach((lineOfWords, index) => {
+      push();
+      const words = Math.max(0, lineOfWords.split(" ").length - 1);
+      const spacer = floor((width - margins - textWidth(lineOfWords)) / words);
+      wordSpacing(spacer * movement);
+      text(lineOfWords, width / 2, textLeading() / 2 + textLeading() * index);
+      push()
+      noFill()
+      translate(width / 2, textLeading() / 2 + textLeading() * index)
+      rect(0,0,textWidth(lineOfWords),textLeading())
+      
+      pop()
+      pop();
     });
-
+  
     linesOfWords.forEach((lineOfWords, index, arr) => {
-        push();
-        const words = Math.max(0, lineOfWords.split("").length - 1);
-        const spacer = floor(
-            (width - margins - textWidth(lineOfWords)) / words
-        );
-        const ctx = drawingContext;
-        letterSpacing(spacer * movement);
-        text(
-            lineOfWords,
-            width / 2,
-            height -
-                (textLeading() / 2 + (arr.length - 1) * textLeading()) +
-                textLeading() * index
-        );
-        pop();
+      push();
+      const words = Math.max(0, lineOfWords.split("").length - 1);
+      const spacer = floor((width - margins - textWidth(lineOfWords)) / words);
+      letterSpacing(spacer * movement);
+      text(
+        lineOfWords,
+        width / 2,
+        height -
+          (textLeading() / 2 + (arr.length - 1) * textLeading()) +
+          textLeading() * index
+      );
+      push()
+      noFill()
+      translate(width / 2, height -
+          (textLeading() / 2 + (arr.length - 1) * textLeading()) +
+          textLeading() * index)
+      rect(0,0,textWidth(lineOfWords),textLeading())
+      
+      pop()
+      pop();
     });
-
+    
     linesOfWords.forEach((lineOfWords, index) => {
-        push();
-        const words = Math.max(0, lineOfWords.split(" ").length - 1);
-        const letters = Math.max(0, lineOfWords.split("").length - 1);
-        const wordsSpacer = floor(
-            (width - margins - textWidth(lineOfWords)) / words
-        );
-        const lettersSpacer = floor(
-            (width - margins - textWidth(lineOfWords)) / letters
-        );
-        const ctx = drawingContext;
-        wordSpacing(wordsSpacer * 0.25 * movement);
-        letterSpacing(lettersSpacer * 0.75 * movement);
-        text(
-            lineOfWords,
-            width / 2,
-            height / 2 + textLeading() / 2 + textLeading() * (index - 1)
-        );
-        pop();
+      push();
+      const words = Math.max(0, lineOfWords.split(" ").length - 1);
+      const letters = Math.max(0, lineOfWords.split("").length - 1);
+      const wordsSpacer = floor((width - margins - textWidth(lineOfWords)) / words);
+      const lettersSpacer = floor((width - margins - textWidth(lineOfWords)) / letters);
+      wordSpacing(wordsSpacer * .25 * movement);
+      letterSpacing(lettersSpacer* .75 * movement);
+      text(lineOfWords, width / 2, height / 2 + textLeading() / 2 + textLeading() * (index-1));
+      push()
+      noFill()
+      translate(width / 2,height / 2 + textLeading() / 2 + textLeading() * (index-1))
+      rect(0,0,textWidth(lineOfWords),textLeading())
+      pop()
+      pop();
     });
-
-    push();
-    noStroke();
-    fill(0);
-    textSize(10);
-    text(nfc(frameRate(), 0), 15, 10);
-    pop();
-}
+    
+    }
+    // uncomment to display frame rate
+    // push();
+    // noStroke();
+    // fill(0);
+    // textSize(10)
+    // text(nfc(frameRate(), 0), 15, 10);
+    // pop();
+  }
+  
+  function ease( p, g){
+    if (p < 0.5) 
+      return 0.5 * pow(2*p, g);
+    else
+      return 1 - 0.5 * pow(2*(1 - p), g);
+  }
+  
